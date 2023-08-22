e20fb9ba885b90703187bca77edb2f35fc577d36
nagasgit <nagarajanmeenakshisundaram@gmail.com>
2023-08-22 16:15:03 +0530

    selenium hw

--- src/test/java/mandatoryHW/selenium/Day4.java
+++ src/test/java/mandatoryHW/selenium/Day4.java
@@ -1,12 +1,17 @@
 package mandatoryHW.selenium;
 
+import static org.testng.Assert.assertEquals;
+
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Day4 extends BaseCls{
@@ -28,13 +33,33 @@ public class Day4 extends BaseCls{
 	 * 
 	 */
 	@Test
-	public void day4()
+	public void day4() throws InterruptedException
 	{
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
 		WebElement viewAll = driver.findElement(By.xpath("//button[text()='View All']"));
 		wait.until(ExpectedConditions.elementToBeClickable(viewAll)).click();
+		
+		driver.findElement(By.xpath("//input[@placeholder='Search apps or items...']")).clear();
+		driver.findElement(By.xpath("//input[@placeholder='Search apps or items...']")).sendKeys("content",Keys.TAB);
+		
+		driver.findElement(By.xpath("//a//span/*/mark[text()='Content']")).click();
+		
+		WebElement chatter = driver.findElement(By.xpath("//a[@title='Chatter']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", chatter);
+		
+		//wait.until(ExpectedConditions.visibilityOf(chatter)).click();
+		Thread.sleep(3000);
+		String title = driver.getTitle();
+		
+		driver.findElement(By.xpath("//span[text()='Question']")).click();
+		driver.findElement(By.xpath("//textarea[contains(@class,'cuf-questionTitleField')]")).sendKeys("hello");
+		driver.findElement(By.xpath("//div[contains(@data-placeholder,'If you have more to say,')]")).sendKeys("i am here to learn programming");
+		driver.findElement(By.xpath("//button[text()='Ask']")).click();
+		String text = driver.findElement(By.xpath("//span[text()='i am here to learn programming']")).getText();
+		Assert.assertEquals(text, "i am here to learn programming");
 	}
 
 }
--- src/test/java/mandatoryHW/week4/Day2_AlternateAppro.java
+++ src/test/java/mandatoryHW/week4/Day2_AlternateAppro.java
@@ -1,12 +1,60 @@
 package mandatoryHW.week4;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Day2_AlternateAppro {
 	
 	/*
 	 * Pseudo Code:
-	 * 1) 
-	 * 
+	 * 1) Get the input array and sort the array
+	 * 2) Create a map and add all the values into it using for loop
+	 * 3) Check if map contains i value and if map.get(i) == 2
+	 * 								then dup = i
+	 * 			else missingVal = i
+	 * 4) return new int[] {dup, missingVal}
 	 * 
 	 */
+	
+	@Test
+	public void test1()
+	{
+		int[] actual = arrMissingVal(new int[] {8,7,5,5,4,3,2,1});
+		System.out.println(Arrays.toString(actual));
+		Assert.assertEquals(actual, new int[] {5,6});
+	}
+	
+	public int[] arrMissingVal(int[] nums)
+	{
+		//Arrays.sort(nums);
+		//System.out.println(Arrays.toString(nums));
+		Map<Integer, Integer> mVal = new HashMap<Integer, Integer>();
+		int d = -1;
+		int m = 1;
+ 		for (int n : nums) {
+			mVal.put(n, mVal.getOrDefault(n, 0)+1);
+			System.out.println(mVal);
+		}
+ 		
+ 		for (int i = 1; i < nums.length; i++) {
+ 			if (mVal.containsKey(i))
+ 			{
+ 				if(mVal.get(i) == 2)
+ 				{
+ 					d = i;
+ 				}
+ 			} else {
+ 				m = i;
+ 			}
+			
+		}
+		
+		return new int[] {d,m};
+	}
 
 }
