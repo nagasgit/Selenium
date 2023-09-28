5a2acc10a8d83ec520b3b1dea910520deead0bfb
nagasgit <nagarajanmeenakshisundaram@gmail.com>
2023-09-28 17:54:31 +0530

    hw

--- src/test/java/mandatoryHW/DSA/week10/BowlingGame_addnlApproach.java
+++ src/test/java/mandatoryHW/DSA/week10/BowlingGame_addnlApproach.java
@@ -3,7 +3,7 @@ package mandatoryHW.DSA.week10;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class BowlingGame 
+public class BowlingGame_addnlApproach 
 {
 	/* Time - O[n]
 	 * Space - O[1]
@@ -12,16 +12,8 @@ public class BowlingGame
 	   2.Frame Test data (valid, invalid, complex and edge cases)
 		  Test Data:
 		  #1:
-		  input: 
-		  output: 
-		  
-		  #2:
-		  input: 
-		  output: 
-		  
-		  #2:
-		  input: 
-		  output: 
+		  input: [4,10,7,9], [6,5,2,3]
+		  output: 1
 		  
 		3.Do you know the Solution?
 		  yes
@@ -29,6 +21,13 @@ public class BowlingGame
 		  yes
 		5.Derive Pseudo code in paper (for the best chosen approach)
 			Pseudo Code:
+				1) Initialize variables => sum1, sum2, p1ind, p2ind as 0
+				2) Iterate the loop for either player 1 or 2 length
+					a) increment the sum with every index values of player 1 & 2
+					b) check if p1ind-- is greater than 0, then increment the player1[i]again to sum
+					c) check if p2ind-- is greater than 0, then increment the player2[i]again to sum
+					d) if player1[i] is equals to 10 then set the p1ind as 2
+					e) if player2[i] is equals to 10 then set the p2ind as 2
 			
 		6.Dry run the pseudo code with all test data from step #2
 		  yes
@@ -53,42 +52,26 @@ public class BowlingGame
 	
 	public int bowlingGame(int[] player1, int[] player2)
 	{
-		int left = 0; int right = 0; 
-		//int sum1 = player1[left]; int sum2 = player2[right];
-		int sum1 = 0; int sum2 = 0;
-		int p1ind = 0; int p2ind = 0;
-		
-		while (left < player1.length && right < player2.length)
-		{
-			if (p1ind == 1)
-				player1[left] = 2 * player1[left];
-			if (p2ind == 1)
-				player2[right] = 2 * player2[right];
-			
-			if (sum1 == sum2)
-			{
-				sum1 = sum1 + player1[left];
-				sum2 = sum2 + player2[right];right++;left++;
-			}else if (sum1 > sum2) {
-				sum2 = sum2 + player2[right];
-				if (player2[right] == 10)
-					p2ind = 1;
-				if (right == player2.length)
-				break;
-				right++;
-			} else {
-				sum1 = sum1 + player1[left];
-				if (player1[left] == 10)
-					p1ind = 1;
-				if (left == player1.length)
-					break;
-				left++;
+		int sum1 = 0;
+		int sum2 = 0;
+		int p1ind = 0;
+		int p2ind = 0;
+		for (int i = 0; i < player2.length; i++) {
+			sum1 += player1[i];
+			sum2 += player2[i];
+			if (p1ind-- > 0) {
+				sum1 += player1[i];
+			}
+			if (p2ind-- > 0) {
+				sum2 += player2[i];
+			}
+			if (player1[i] == 10) {
+				p1ind = 2;
+			}
+			if (player2[i] == 10) {
+				p2ind = 2;
 			}
 		}
-		if (sum1 > sum2)
-		return 1;
-		else
-		return 2;
-		
+		return sum1 > sum2 ? 1 : sum2 > sum1 ? 2 : 0;
 	}
 }
